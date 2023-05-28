ef253988129a7986d0d51c6c6b480d317582a9c6
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-05-28 14:04:37 -0400

    deleted content view, made new groups and MoviesListView

---
+++ FavouriteMovies/Views/MoviesListView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesListView.swift
+//  FavouriteMovies
+//
+//  Created by Graeme Armstrong on 2023-05-28.
+//
+
+import SwiftUI
+
+struct MoviesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesListView()
+    }
+}
