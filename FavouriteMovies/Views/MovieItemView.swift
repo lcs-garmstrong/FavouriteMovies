1c4e8473d1a0d51922e4e51e04cb53141df860e0
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-05-28 14:33:31 -0400

    Created a static layout for the app.

---
+++ FavouriteMovies/Views/MovieItemView.swift
@@ -0,0 +1,39 @@
+//
+//  MovieItemView.swift
+//  FavouriteMovies
+//
+//  Created by Graeme Armstrong on 2023-05-28.
+//
+
+import SwiftUI
+
+struct MovieItemView: View {
+    // MARK: Stored properties let name: String
+    let name: String
+    let genre: String
+    let rating: Int
+    
+    // MARK: Computed properties
+    // The user interface
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title3)
+                    .bold()
+                Text(genre)
+                    .font (.subheadline)
+            }
+            Spacer()
+            Text("\(rating)/5")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+struct MovieItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieItemView(name: "Raiders of the Lost Ark", genre: "Action", rating: 5)
+    }
+}
